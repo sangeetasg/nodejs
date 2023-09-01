b507f3a5c741f99ecf67a3b11b132f8343bd2f23
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-09-01 16:15:49 +0100

    updated the file

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+let app = require("express");
+let express = app();
+
+express.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+express.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
