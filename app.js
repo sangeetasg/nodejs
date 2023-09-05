8711cac06fc378ba5b778a09837fd4cca58c6360
Sangeeta Yadav <sangeeta.yadav@joshsoftware.com>
2023-09-05 09:45:03 +0100

    Completed assignment 2

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-let app = require("express");
-let express = app();
+// let app = require("express");
+// let express = app();
 
-express.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// express.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-express.listen(3000, () => {
-  console.log(`Example app listening on port 3000`)
-})
+// express.listen(3000, () => {
+//   console.log(`Example app listening on port 3000`)
+// })
+
+const http = require('http');
+
+const hostname = '127.0.0.1'
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
